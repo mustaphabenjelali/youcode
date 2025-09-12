18677cbc1f80d5a5669835e859f309ff1c26adb8
Mustapha Benjelali <mustaphabenjelali400@gmail.com>
2025-09-12 12:40:17 +0200

    Updated code

--- STRUCTURE/Challenge5/main.c
+++ STRUCTURE/Challenge5/main.c
@@ -8,7 +8,22 @@ struct Livre {
     int annee;
 };
 
+struct Livre creelivre(const char *titre,const char *auteur,int annee){
+    struct Livre li;
+
+    strcpy(li.titre,titre);
+    strcpy(li.auteur,auteur);
+    li.annee = annee;
+    return li;
+};
+
 int main() {
+
+    struct Livre livreli=creelivre("Nodor ino","rachid nadori",2025);
+
+    printf("Titre est : %s\n",livreli.titre);
+    printf("Auteur est : %s\n",livreli.auteur);
+    printf("annee de production : %d\n",livreli.annee);
 
     return 0;
 }
